9649364688a919ef6fbdfec7e6d96b2a61370530
skycader <skycader@github.com>
2023-03-06 01:53:12 +0300

    post page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-post-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent {
+  constructor(private route: ActivatedRoute) {
 
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params)
+    })
+    console.log(this.route)
+  }
 }
