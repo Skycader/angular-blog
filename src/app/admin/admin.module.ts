5bdaff9f17a9890114f5e00f175e4f9d070f7859
skycader <skycader@yandex.ru>
2023-09-24 02:28:27 +0300

    lesson 7 in progress

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,7 +6,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,18 +14,24 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     LoginPageComponent,
     DashboardPageComponent,
     CreatePageComponent,
-    EditPageComponent
+    EditPageComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
-      {path: '', component: AdminLayoutComponent, children: [
-        {path: 'login', component: LoginPageComponent},
-        {path: 'dashboard', component: DashboardPageComponent},
-        {path: 'create', component: CreatePageComponent},
-        {path: 'post/:id/edit', component: EditPageComponent},
-      ]}
-    ])
-  ]
+      {
+        path: '',
+        component: AdminLayoutComponent,
+        children: [
+          { path: 'login', component: LoginPageComponent },
+          { path: 'dashboard', component: DashboardPageComponent },
+          { path: 'create', component: CreatePageComponent },
+          { path: 'post/:id/edit', component: EditPageComponent },
+        ],
+      },
+    ]),
+  ],
 })
 export class AdminModule { }
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+  public form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
 
+  constructor() { }
+
+  ngOnInit() { }
+  submit() {
+    console.log('submit');
+  }
 }
