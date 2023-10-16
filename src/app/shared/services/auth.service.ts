33fa69793ee7be3f2a2a4072ca0e90302fa8b79f
skycader <skycader@yandex.ru>
2023-10-16 07:53:47 +0300

    lesson 10 complete

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUser } from 'src/app/shared/models/user.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +12,10 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   public login(user: IUser): Observable<any> {
-    return this.http.post('', user);
+    return this.http.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,
+      user,
+    );
   }
 
   public logout() { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyDs_AvziAAuJ1q1lyfkWOeNWGq4HY-AfW8',
+};
