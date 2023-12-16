5c49c95204842d6a43cc5b3036e162cb78f15420
skycader <skycader@yandex.ru>
2023-12-16 22:02:06 +0300

    added_pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +13,7 @@ import { AuthInterceptor } from './shared/auth.interceptor';
 import ruLocale from '@angular/common/locales/ru';
 import { registerLocaleData } from '@angular/common';
 import { QuillModule } from 'ngx-quill';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 registerLocaleData(ruLocale, 'ru');
 
@@ -31,7 +32,12 @@ const INTERCEPTOR_PROVIDER = {
     NotFoundComponent,
     PostComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, QuillModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, QuillModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent],
 })
